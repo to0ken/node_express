9a586028574065c5c31c03c5996ad49d541ba665
to0ken <evgeniamafinova@gmail.com>
2026-04-20 08:59:19 +0300

    Create register.js

---
+++ public/register.js
@@ -0,0 +1,20 @@
+
+async function login(){
+    const password = document.getElementById('password').value;
+    const login = document.getElementById('login').value;
+
+    const resp = await fetch("/api/login",{
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({password, login}),
+    })
+
+    const data = await resp.json();
+    if (data.status === "success"){
+        window.location.href ="/dashboard";
+    }
+    if (data.status === "error"){
+        window.location.href ="/error"
+    }
+
+}
