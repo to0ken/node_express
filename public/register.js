678c53795acae271ca8ea655ad9e08fd0b5666b9
to0ken <evgeniamafinova@gmail.com>
2026-04-21 12:45:43 +0300

    Update register.js

--- public/register.js
+++ public/register.js
@@ -18,3 +18,23 @@ async function login(){
     }
 
 }
+
+async function password() {
+    const password = document.getElementById("password").value;
+    const login = document.getElementById("login").value;
+
+    const resp = await fetch("/api/register", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ password, login }),
+    });
+
+    const data = await resp.json();
+    if (data.status === "success") {
+      window.location.href = "/dashboard";
+    }
+    if (data.status === "error") {
+      window.location.href = "/error";
+    }
+
+}
