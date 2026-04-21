e3e2d30826dd183a215b677ee8aaea2d07f4651b
to0ken <evgeniamafinova@gmail.com>
2026-04-21 12:31:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {initDatabase} from "database/database"
+import {initDatabase} from "./database/database.js"
 
 import apiRoutes from "./routes/api.js"
 import pageRoutes from "./routes/pages.js"
